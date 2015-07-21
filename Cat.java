738cf0edd3601d2d00b2c51d968db1b866e869be
ProgSelC <ProgSelC@users.noreply.github.com>
2015-07-21 22:04:48 +0300

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,44 +1,93 @@
 package lessons;
 
-public class Car {
-	String color;
-	double weight;
-	int year;
-	private double velosity = 0;
+import java.util.Random;
 
-	Car(String color, double weight, int year) {
-		this.color = color;
+public class Cat {
+	private static int catCount;
+
+	private String name;
+	private int age;
+	private double weight;
+	private String sex;
+
+	public static int getCatCount() {
+		return catCount;
+	}
+
+	public Cat(String name, int age, double weight, String sex) {
+		catCount++;
+		this.name = name;
+		this.age = age;
 		this.weight = weight;
-		this.year = year;
+		this.sex = sex;
 	}
 
-	Car() {
+	public Cat(String name) {
+		catCount++;
+		this.name = name;
+		this.age = 1;
+		this.weight = 10;
+		this.sex = "male";
 	}
 
-	Car(int year) {
-		this.color = "white";
-		this.weight = 1600;
-		this.year = year;
+	public String getName() {
+		return name;
 	}
 
-	void beep() {
-		System.out.println("BEEP-BEEP!!!");
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	void acceleration(double a) {
-		velosity = velosity + a;
+	public int getAge() {
+		return age;
 	}
 
-	void deceleration(double b) {
-		if (velosity - b >= 0)
-			velosity = velosity - b;
+	public void setAge(int age) {
+		this.age = age;
 	}
 
-	void print() {
-		System.out.println("Color: " + color);
-		System.out.println("Weight: " + weight);
-		System.out.println("Year car: " + year);
-		System.out.println("Velosity: " + velosity);
-		System.out.println();
+	public double getWeight() {
+		return weight;
 	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public void voice() {
+		System.out.println(this.name+" says Meow!");
+	}
+
+	public void compareCats(Cat otherCat) {
+		if (this.age > otherCat.getAge()) {
+			System.out.printf("%s is older then %s\n", this.name, otherCat.getName());
+		} else if (this.age < otherCat.getAge()) {
+			System.out.printf("%s is older then %s\n", otherCat.getName(), this.name);
+		} else {
+			System.out.printf("%s and %s have same age!\n", this.name, otherCat.getName());
+		}
+	}
+	
+	public Cat newCat (Cat otherCat){
+		if(this.sex != otherCat.getSex()){
+			Random rn = new Random();
+			Cat littleCat = new Cat("Noname", 0, 0.1, (rn.nextBoolean())?"male":"female");
+			return littleCat;
+		} if (this.sex.matches("male")){
+			System.out.printf("%s and %s both male! Fight!\n", this.name, otherCat.getName());
+			return null;
+		}else {
+			System.out.printf("%s and %s have same sex! Sorry, no kittens\n", this.name, otherCat.getName());
+			return null;
+		}
+	}
+
 }
